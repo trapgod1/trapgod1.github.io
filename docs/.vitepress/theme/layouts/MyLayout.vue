959ee31479619a1a079c617584bb0c5072093a7f
Unkn0wnWasTaken <arfifa00@gmail.com>
2024-05-27 17:13:44 +0100

    Test

---
+++ docs/.vitepress/theme/layouts/MyLayout.vue
@@ -0,0 +1,12 @@
+<!--.vitepress/theme/MyLayout.vue-->
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+  
+  <script>
+  export default {
+
+  };
+  </script>
